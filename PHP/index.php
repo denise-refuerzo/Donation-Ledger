a54b480efa2fe901b86e0acc60465f90aebfbbd2
Ralph Lucero <ralphluceroxx@gmail.com>
2025-05-06 19:39:47 +0800

    added daily donations

--- PHP/index.php
+++ PHP/index.php
@@ -37,7 +37,8 @@ if ($session->getRole() !== 'admin') {
 
   <header class="bg-dark text-white p-3 d-flex justify-content-between align-items-center">
     <h1 class="h4 m-0">Donation Ledger</h1>
-    <a href="addDonation.php" class="btn btn-light text-dark">Donate</a>
+    <a href="addDonation.php" class="btn btn-light text-dark me-2">Donate</a>
+    <a href="../PHP/dailydonation.php" class="btn btn-light text-dark me-2">Daily Donations</a>
     <?php if (!$isLoggedIn): ?>
         <a href="Register.php" class="btn btn-light text-dark">Register</a>
     <?php else: ?>  
