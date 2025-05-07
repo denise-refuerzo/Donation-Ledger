e052a13936e67bdcf7e6d0f792b5dc81ff3bddaf
Alex <alexrefuerzocapute@gmail.com>
2025-05-08 00:03:21 +0800

    Merge Fixed

--- adddonation.sql
+++
@@ -1,6 +0,0 @@
-DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `displaydonation`()
-BEGIN
-select sum(c.amount) as cashamount,date(d.timestamp) as donationdate from cash c inner join donations d on c.donation_id=d.donation_id group by DATE(d.timestamp);
-END$$
-DELIMITER ;
